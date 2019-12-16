33ad629dd1e3c78e90cea86e5ec9ba4e22813aee
Rytis Petronis <36188103+Rytisgit@users.noreply.github.com>
2019-12-16 22:38:00 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,8 +3,22 @@ import hashlib
 import codecs
 import struct
 
+
+def Compare():
+    p = RawProxy()
+    blockHash = p.getblockhash(input("block height:"))
+    block = p.getblock(blockHash)
+    hex = (Endian(block['version']) + ReOrder(block['previousblockhash']) + ReOrder(block['merkleroot']) + Endian(
+        block['time']) + ReOrder(block['bits']) + Endian(block['nonce']))
+    FullHash = codecs.decode(
+        codecs.encode(hashlib.sha256(hashlib.sha256(codecs.decode(hex, 'hex')).digest()).digest()[::-1], 'hex_codec'))
+    print("Block Hash", blockHash)
+    print("Verif Hash", FullHash)
+    print("Same : ", blockHash == FullHash)
+
 def Endian(Numb):
     return codecs.encode(struct.pack("<I", Numb), 'hex').decode()
+
 
 def ReOrder(Line):
     Swapper = list(Line[::-1])
@@ -13,14 +27,5 @@ def ReOrder(Line):
         Swapped = Swapped + (Swapper[i + 1] + Swapper[i])
     return Swapped
 
-def Compare():
-    p = RawProxy()
-    blockHash = p.getblockhash(input("block height:"))
-    block = p.getblock(blockHash)
-    hex = (Endian(block['version']) + ReOrder(block['previousblockhash']) + ReOrder(block['merkleroot']) + Endian(block['time']) + ReOrder(block['bits']) + Endian(block['nonce']))
-    FullHash = codecs.decode(codecs.encode(hashlib.sha256(hashlib.sha256(codecs.decode(hex, 'hex')).digest()).digest()[::-1], 'hex_codec'))
-    print("Block Hash", blockHash)
-    print("Verif Hash", FullHash)
-    print("Same : ", blockHash == FullHash)
 
 Compare()
